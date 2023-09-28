d92c39192863719dcf48f0664897e8131e1c5354
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-09-27 21:30:54 -0300

    Utilizando bootstrap com o react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
 
 export default function Footer(){
     return(
         <footer>
-            <h1>Sou um rodapé</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <h4>Senac</h4>
+                        <p>2023 - Todos os direitos reservados</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h4>Características</h4>
+                        <ul className="list-group">
+                            <li className="list-group-item"><Link to= ""></Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,8 +1,16 @@
 
 export default function HomeScreen(){
     return(
-        <div>
-            <h1>Sou a página inicial</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h1>Sou uma página inicial</h1>
+                    <div className="row">
+                        <div className="col-md-6">1</div>
+                        <div className="col-md-6">2</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
